8e325a3d145d3327af373608397411c6832ef9e3
aleky63 <ab63@list.ru>
2023-12-21 18:35:54 +0300

    second21_12

--- js/main.js
+++ js/main.js
@@ -1,6 +1,14 @@
-const menuBtn = document.querySelector('.menu__btn')
-const menuBurger = document.querySelector('.header__menu-list')
+const menuBtn = document.querySelector(".menu__btn");
+const menuBurger = document.querySelector(".header__menu-list");
 
-menuBtn.addEventListener('click',()=>{
-    menuBurger.classList.toggle('menu--open')
-})
+menuBtn.addEventListener("click", () => {
+  menuBurger.classList.toggle("menu--open");
+});
+
+const swiperOne = new Swiper(".feedback__slider", {
+  loop: true,
+
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
